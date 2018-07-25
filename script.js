4c2533c3d4d9e0e08f569f6eb27f56c49714fe2c
Marian Dörk <md@mariandoerk.de>
2018-07-25 11:32:53 +0200

    readme updated; scripts combined

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // christopher pietsch 2018
 // cpietsch@gmail.com
 
+// TEXTURES
+
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
@@ -9,7 +11,7 @@ const argv = require('minimist')(process.argv.slice(2));
 const execFile = require('child_process').execFile;
 const localPath = i => path.relative(process.cwd(), i)
 
-console.dir('starting with' argv);
+console.dir('starting with', argv);
 
 const textureRes1 = argv.s || 256;
 const textureRes2 = 1024;
@@ -46,7 +48,7 @@ glob(inputPath + '/*.' + inputFormat, function (er, files) {
 
 	sequence.then((s) => {
 		console.log('image res generated successfully')
-		console.log('please process to the spritesheet script')
+		// console.log('please process to the spritesheet script')
 	})
 })
 
@@ -73,3 +75,60 @@ function convert (buffer, path, res){
 			})
 	})
 }
+
+
+// SPRITESHEETS
+
+const PixiPacker = require("pixi-packer");
+const jpegTran = require("imagemin-jpegtran");
+// const argv = require('minimist')(process.argv.slice(2));
+// const path = require("path");
+
+// const inputPath = argv.i;
+// const workPath = path.join(path.dirname(inputPath), 'vv-data');
+const pngPath = path.join(workPath, '256');
+const outPath = path.join(workPath, 'sprites');
+const cachePath = path.join(workPath, 'tmp');
+
+var config = {
+    scales: {
+        "full": {"scale": 1, "resolution": 1}
+    },
+    variations: ["web"],
+    loading_stages: [ "web" ],
+    group_default: {
+        max_width: 2048,          // default: 2048
+        max_height: 2048,         // default: 1024
+        oversized_warning: true, // default: false
+        padding: 1               // default: 1
+    },
+    "groups": [
+        {
+            "id": "web",
+            "loading_stage": "web",
+            // "compressor": pngQuant(),
+            "compressor": jpegTran(),
+            "jpeg": true,
+            "quality": 60,
+            "sprites": ['*png']
+        }
+    ],
+    // cleanCache: true,
+    cleanOutput: true,
+    trim: true
+}
+
+var pixiPacker = new PixiPacker(
+    config,
+    path.resolve(__dirname, pngPath),
+    path.resolve(__dirname, outPath),
+    cachePath
+);
+
+pixiPacker.process()
+	.catch(err => {
+	    console.error("Error:", err.stack);
+	})
+	.then(a => {
+		console.log('sprites generated sucessfully')
+	})
